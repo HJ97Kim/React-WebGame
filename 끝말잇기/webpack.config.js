5df71ed9e22ac4bd42eaba5a0d4faf9341135d6d
Hyeongjin Kim <kimjin5537@daum.net>
2020-11-04 22:28:04 +0900

    끝말잇기(Class)

---
+++ 끝말잇기/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  entry: {
+    app: ['./client'],
+  }, // 입력
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'],
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  },
+
+  plugins: [],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  }, // 출력
+};
